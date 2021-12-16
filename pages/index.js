6f1164051933cb03f73bee0d9c1aa02572edf2ee
Sam Donaghy-Bell <sam@reflex-studios.com>
2021-12-16 11:37:19 +0000

    setting max loan amounts dependning on type of loan selected

--- pages/index.js
+++ pages/index.js
@@ -55,249 +55,219 @@ export default function Home() {
               </select>
             </div>
 
-
-
-              <div className="flex flex-col gap-y-10">
-                <div className="flex flex-col gap-y-6">
-                  <div className="flex justify-between w-full">
-                    <label
-                      htmlFor="loanAmount"
-                      className="text-1xs opacity-50 font-acumin font-light"
+            <div className="flex flex-col gap-y-10">
+              <div className="flex flex-col gap-y-6">
+                <div className="flex justify-between w-full">
+                  <label
+                    htmlFor="loanAmount"
+                    className="text-1xs opacity-50 font-acumin font-light"
+                  >
+                    Loan Amount
+                  </label>
+                  <div className="flex">
+                    <span>£</span>
+                    <span
+                      id="amountInput"
+                      className="text-white w-[44px] bg-transparent"
                     >
-                      Loan Amount
-                    </label>
-                    <div className="flex">
-                      <span>£</span>
-                      <span
-                        id="amountInput"
-                        className="text-white w-[44px] bg-transparent"
-                      >
-                        {loanAmount}
-                      </span>
-                    </div>
-                  </div>
-                </div>
-
-                {loanType === "Personal Loan up to £7,500" ? (<input
-                  onChange={(e) => setLoanAmount(e.target.value)}
-                  type="range"
-                  name="rangeInput"
-                  className="range rounded-lg bg-primary h-[10px] w-full"
-                  min="100"
-                  max="7500"
-                  step="100"
-                  value={loanAmount}
-                />) : null }
-                {loanType === "Personal Loan £7,501 to £15,000" ? (<input
-                  onChange={(e) => setLoanAmount(e.target.value)}
-                  type="range"
-                  name="rangeInput"
-                  className="range rounded-lg bg-primary h-[10px] w-full"
-                  min="7500"
-                  max="15000"
-                  step="100"
-                  value={loanAmount}
-                />) : null }
-                {loanType === "Personal Loan £15,000 to £25,000" ? (<input
-                  onChange={(e) => setLoanAmount(e.target.value)}
-                  type="range"
-                  name="rangeInput"
-                  className="range rounded-lg bg-primary h-[10px] w-full"
-                  min="1500"
-                  max="25000"
-                  step="100"
-                  value={loanAmount}
-                />) : null }
-                {loanType === "Secured / Share Secured Personal Loan up to £25,000" ? (<input
-                  onChange={(e) => setLoanAmount(e.target.value)}
-                  type="range"
-                  name="rangeInput"
-                  className="range rounded-lg bg-primary h-[10px] w-full"
-                  min="1500"
-                  max="25000"
-                  step="100"
-                  value={loanAmount}
-                />) : null }
-                
-
-                <div className="flex flex-col gap-y-6">
-                  <div className="flex justify-between w-full">
-                    <label
-                      htmlFor="loanTerm"
-                      className="text-1xs opacity-50 font-acumin font-light"
-                    >
-                      Loan Term
-                    </label>
-                    <div className="flex">
-                      <span
-                        id="termInput"
-                        className="text-white w-[20px] bg-transparent"
-                      >
-                        {loanTerm}
-                      </span>
-                      <span> months </span>
-                      <span
-                        id="termInput"
-                        className="text-white w-[33px] bg-transparent"
-                      >
-                        ({loanTermYears.toFixed(1)}
-                      </span>
-                      <span> years)</span>
-                    </div>
-                  </div>
-                </div>
-
-                <input
-                  onChange={(e) => setLoanTerm(e.target.value)}
-                  type="range"
-                  name="termInput"
-                  className="range rounded-lg bg-primary h-[10px] w-full"
-                  min="2"
-                  max="60"
-                  step="1"
-                  value={loanTerm}
-                />
-
-                <div className="flex flex-col md:flex-row justify-between w-full text-center   border-b-2 border-opacity-10">
-                  <div className="w-full md:w-1/4 py-6 md:py-28 flex flex-col">
-                    <p className="opacity-30 text-mobileNav">
-                      Total you'll pay
-                    </p>
-                    <p className="text-footerNav">£1,071.82</p>
-                  </div>
-
-                  <div className="flex-1 order-first md:order-none paymentSchedule">
-                    <div className="">
-                      <ul className="flex justify-around md:justify-between gap-x-4">
-                        <li
-                          className={`pb-[5px] ${
-                            schedule !== "monthly"
-                              ? "opacity-30"
-                              : "border-b-[3px]"
-                          }`}
-                        >
-                          <button
-                            className="cursor-pointer"
-                            onClick={(e) => updateSchedule(e, "monthly")}
-                          >
-                            Monthly
-                          </button>
-                        </li>
-                        <li
-                          className={`pb-[5px] ${
-                            schedule !== "fortnightly"
-                              ? "opacity-30"
-                              : "border-b-[3px]"
-                          }`}
-                        >
-                          <button
-                            className="cursor-pointer"
-                            onClick={(e) => updateSchedule(e, "fortnightly")}
-                          >
-                            Fortnightly
-                          </button>
-                        </li>
-                        <li
-                          className={`pb-[5px] ${
-                            schedule !== "weekly"
-                              ? "opacity-30"
-                              : "border-b-[3px]"
-                          }`}
-                        >
-                          <button
-                            className="cursor-pointer"
-                            onClick={(e) => updateSchedule(e, "weekly")}
-                          >
-                            Weekly
-                          </button>
-                        </li>
-                      </ul>
-
-                      <div className="w-full flex justify-center items-center mt-6">
-                        {schedule === "monthly" ? (
-                          <div className="w-[200px] h-[200px] bg-lightBlue rounded-full flex flex-col items-center justify-center ">
-                            <p className="opacity-30 text-mobileNav">
-                              Monthly Repairments
-                            </p>
-                            <p className="text-3xl">
-                              £{(repayable / loanTerm).toFixed(2)}
-                            </p>
-                          </div>
-                        ) : null}
-
-                        {schedule === "fortnightly" ? (
-                          <div className="w-[200px] h-[200px] bg-lightBlue rounded-full flex flex-col items-center justify-center ">
-                            <p className="opacity-30 text-mobileNav">
-                              Fortnightly Repairments
-                            </p>
-                            <p className="text-3xl">
-                              £{(repayable / (loanTerm * 2)).toFixed(2)}
-                            </p>
-                          </div>
-                        ) : null}
-
-                        {schedule === "weekly" ? (
-                          <div className="w-[200px] h-[200px] bg-lightBlue rounded-full flex flex-col items-center justify-center ">
-                            <p className="opacity-30 text-mobileNav">
-                              Weekly Repairments
-                            </p>
-                            <p className="text-3xl">
-                              £{(repayable / (loanTerm * 4)).toFixed(2)}
-                            </p>
-                          </div>
-                        ) : null}
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="w-full md:w-1/4 py-6 md:py-28 flex flex-col">
-                    <p className="opacity-30 text-mobileNav">Interest</p>
-                    <p className="text-footerNav">£71.82</p>
-                  </div>
-                </div>
-                <div className="flex justify-between items-center w-full">
-                  <p className="text-1xs">6.05% APR (£100 - £25,000)</p>
-                  <div className="flex justify-start overflow-hidden">
-                    <a
-                      href="{{ button_link }}"
-                      className="rounded-full bg-secondary min-w-max py-[16px] px-[24px] text-smallTitle leading-squat text-white flex items-center group"
-                    >
-                      <svg
-                        width="8"
-                        height="6"
-                        viewBox="0 0 8 6"
-                        fill="none"
-                        className="transform -rotate-90 -translate-x-20 group-hover:translate-x-0 transition"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M4 5.5L8 1.5L7 0.5L4 3.5L1 0.5L0 1.5L4 5.5Z"
-                          fill="white"
-                        />
-                      </svg>
-                      <button className="uppercase transform group-hover:translate-x-2 transition">
-                        Apply Now
-                      </button>
-                      <svg
-                        width="8"
-                        height="6"
-                        viewBox="0 0 8 6"
-                        fill="none"
-                        className="transform -rotate-90 ml-2 translate-x-0 group-hover:translate-x-20 transition"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M4 5.5L8 1.5L7 0.5L4 3.5L1 0.5L0 1.5L4 5.5Z"
-                          fill="white"
-                        />
-                      </svg>
-                    </a>
+                      {loanAmount}
+                    </span>
                   </div>
                 </div>
               </div>
 
+              <input
+                onChange={(e) => setLoanAmount(e.target.value)}
+                type="range"
+                name="rangeInput"
+                className="range rounded-lg bg-primary h-[10px] w-full"
+                min="100"
+                max={`${
+                  loanType === "Personal Loan up to £7,500"
+                    ? "7500"
+                    : loanType === "Personal Loan £7,501 to £15,000"
+                    ? "15000"
+                    : loanType === "Personal Loan £15,000 to £25,000"
+                    ? "25000"
+                    : "25000"
+                }`}
+                step="100"
+                value={loanAmount}
+              />
 
+              <div className="flex flex-col gap-y-6">
+                <div className="flex justify-between w-full">
+                  <label
+                    htmlFor="loanTerm"
+                    className="text-1xs opacity-50 font-acumin font-light"
+                  >
+                    Loan Term
+                  </label>
+                  <div className="flex">
+                    <span
+                      id="termInput"
+                      className="text-white w-[20px] bg-transparent"
+                    >
+                      {loanTerm}
+                    </span>
+                    <span> months </span>
+                    <span
+                      id="termInput"
+                      className="text-white w-[33px] bg-transparent"
+                    >
+                      ({loanTermYears.toFixed(1)}
+                    </span>
+                    <span> years)</span>
+                  </div>
+                </div>
+              </div>
 
+              <input
+                onChange={(e) => setLoanTerm(e.target.value)}
+                type="range"
+                name="termInput"
+                className="range rounded-lg bg-primary h-[10px] w-full"
+                min="2"
+                max="60"
+                step="1"
+                value={loanTerm}
+              />
+
+              <div className="flex flex-col md:flex-row justify-between w-full text-center   border-b-2 border-opacity-10">
+                <div className="w-full md:w-1/4 py-6 md:py-28 flex flex-col">
+                  <p className="opacity-30 text-mobileNav">Total you'll pay</p>
+                  <p className="text-footerNav">£1,071.82</p>
+                </div>
+
+                <div className="flex-1 order-first md:order-none paymentSchedule">
+                  <div className="">
+                    <ul className="flex justify-around md:justify-between gap-x-4">
+                      <li
+                        className={`pb-[5px] ${
+                          schedule !== "monthly"
+                            ? "opacity-30"
+                            : "border-b-[3px]"
+                        }`}
+                      >
+                        <button
+                          className="cursor-pointer"
+                          onClick={(e) => updateSchedule(e, "monthly")}
+                        >
+                          Monthly
+                        </button>
+                      </li>
+                      <li
+                        className={`pb-[5px] ${
+                          schedule !== "fortnightly"
+                            ? "opacity-30"
+                            : "border-b-[3px]"
+                        }`}
+                      >
+                        <button
+                          className="cursor-pointer"
+                          onClick={(e) => updateSchedule(e, "fortnightly")}
+                        >
+                          Fortnightly
+                        </button>
+                      </li>
+                      <li
+                        className={`pb-[5px] ${
+                          schedule !== "weekly"
+                            ? "opacity-30"
+                            : "border-b-[3px]"
+                        }`}
+                      >
+                        <button
+                          className="cursor-pointer"
+                          onClick={(e) => updateSchedule(e, "weekly")}
+                        >
+                          Weekly
+                        </button>
+                      </li>
+                    </ul>
+
+                    <div className="w-full flex justify-center items-center mt-6">
+                      {schedule === "monthly" ? (
+                        <div className="w-[200px] h-[200px] bg-lightBlue rounded-full flex flex-col items-center justify-center ">
+                          <p className="opacity-30 text-mobileNav">
+                            Monthly Repairments
+                          </p>
+                          <p className="text-3xl">
+                            £{(repayable / loanTerm).toFixed(2)}
+                          </p>
+                        </div>
+                      ) : null}
+
+                      {schedule === "fortnightly" ? (
+                        <div className="w-[200px] h-[200px] bg-lightBlue rounded-full flex flex-col items-center justify-center ">
+                          <p className="opacity-30 text-mobileNav">
+                            Fortnightly Repairments
+                          </p>
+                          <p className="text-3xl">
+                            £{(repayable / (loanTerm * 2)).toFixed(2)}
+                          </p>
+                        </div>
+                      ) : null}
+
+                      {schedule === "weekly" ? (
+                        <div className="w-[200px] h-[200px] bg-lightBlue rounded-full flex flex-col items-center justify-center ">
+                          <p className="opacity-30 text-mobileNav">
+                            Weekly Repairments
+                          </p>
+                          <p className="text-3xl">
+                            £{(repayable / (loanTerm * 4)).toFixed(2)}
+                          </p>
+                        </div>
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="w-full md:w-1/4 py-6 md:py-28 flex flex-col">
+                  <p className="opacity-30 text-mobileNav">Interest</p>
+                  <p className="text-footerNav">£71.82</p>
+                </div>
+              </div>
+              <div className="flex justify-between items-center w-full">
+                <p className="text-1xs">6.05% APR (£100 - £25,000)</p>
+                <div className="flex justify-start overflow-hidden">
+                  <a
+                    href="{{ button_link }}"
+                    className="rounded-full bg-secondary min-w-max py-[16px] px-[24px] text-smallTitle leading-squat text-white flex items-center group"
+                  >
+                    <svg
+                      width="8"
+                      height="6"
+                      viewBox="0 0 8 6"
+                      fill="none"
+                      className="transform -rotate-90 -translate-x-20 group-hover:translate-x-0 transition"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 5.5L8 1.5L7 0.5L4 3.5L1 0.5L0 1.5L4 5.5Z"
+                        fill="white"
+                      />
+                    </svg>
+                    <button className="uppercase transform group-hover:translate-x-2 transition">
+                      Apply Now
+                    </button>
+                    <svg
+                      width="8"
+                      height="6"
+                      viewBox="0 0 8 6"
+                      fill="none"
+                      className="transform -rotate-90 ml-2 translate-x-0 group-hover:translate-x-20 transition"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M4 5.5L8 1.5L7 0.5L4 3.5L1 0.5L0 1.5L4 5.5Z"
+                        fill="white"
+                      />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
           </form>
         </div>
 
